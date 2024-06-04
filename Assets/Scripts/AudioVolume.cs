f5d7905fa28ba3e2b1ca6a02474ad2012a556227
Felix <xfelixe@gmail.com>
2024-06-04 11:39:32 +0200

    Sounds fixed

--- Assets/Scripts/AudioVolume.cs
+++ Assets/Scripts/AudioVolume.cs
@@ -12,6 +12,10 @@ public class AudioVolume : MonoBehaviour
         _slider.value = AudioListener.volume;
         AudioManager.instance.ChangeMasterVolume(_slider.value);
         _slider.onValueChanged.AddListener(Val => AudioManager.instance.ChangeMasterVolume(Val));
+
+
+        AudioManager.instance.ChangeMasterVolume(.5f);
+        _slider.value = AudioListener.volume;
     }
 
 }
